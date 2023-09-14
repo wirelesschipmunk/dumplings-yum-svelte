9134e8a0ec88d56ed1bd96a359a5544bfc51560a
wirelesschipmunk <thomasisaac284@gmail.com>
2023-09-13 20:06:39 -0600

    Added fancyImg component

---
+++ src/lib/FancyImg.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let src;
+	export let alt;
+</script>
+
+<img {src} {alt} />
+
+<style>
+	img {
+		width: 450px;
+		box-shadow: 1px 1px 15px;
+		border-radius: 30px;
+		transition: all 0.5s;
+		cursor: pointer;
+		margin-bottom: 2em;
+	}
+
+	img:hover {
+		width: 500px;
+		transform: rotate(5deg);
+	}
+
+	img:active {
+		width: 480px;
+		transform: rotate(2deg);
+	}
+</style>
